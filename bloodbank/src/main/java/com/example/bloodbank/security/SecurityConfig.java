5329c154bd0cfdc23899f0d338e5c6ee54bc974d
DhanushNadar <3701dhanushsvjc@gmail.com>
2026-04-29 01:08:17 +0530

    Add: docker

--- bloodbank/src/main/java/com/example/bloodbank/security/SecurityConfig.java
+++ bloodbank/src/main/java/com/example/bloodbank/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
